0007d72382482e8064a35473dd91f5dc3200184d
unknown <dcpg95@gmail.com>
2026-01-07 10:23:08 +0900

    refactor: devices 컨트롤러 반환 타입 정리

--- src/domain/devices/devices.controller.ts
+++ src/domain/devices/devices.controller.ts
@@ -1,13 +1,16 @@
-import { AdbService } from './../adb/adb.service';
 import { Controller, Get } from '@nestjs/common';
+import { AdbService } from '../adb/adb.service';
+import { DeviceDetail } from '../adb/interfaces/device-detail.interface';
 
 @Controller('devices')
 export class DevicesController {
   constructor(private readonly adbService: AdbService) {}
 
+  /**
+   * USB 장치 모니터링을 시작하고 단말 상태 목록을 반환합니다.
+   */
   @Get()
-  async devicesGet() {
-    const usb = await this.adbService.device();
-    return usb;
+  async getDevices(): Promise<DeviceDetail[]> {
+    return this.adbService.device();
   }
 }
